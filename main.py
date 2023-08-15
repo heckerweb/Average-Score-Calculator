96f6f2f8168f3ffb128ceb91ad35c8a6f693e959
ingorJesus <109970325+heckerweb@users.noreply.github.com>
2023-08-15 20:08:04 +0700

    Big updates to main.py !

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def main(): # Hàm main
         print("3. Mon Tieng Anh")
         print("4. Thoat")
 
+
+    def chon_hk(mon):
+        print("1. Tinh diem TB mon " + mon + " HK1.")
+        print("2. Tinh diem TB mon " + mon + " HK2.")
+        print("3. Quay tro lai")
+
+
+
     def xep_loai(mon , tbm): # Xếp loại
         gioi = "Chuc mung, ban duoc xep vao loai gioi trong mon " + mon + " !"
         kha = "Ban chi can co gang them chut nua thoi, ban se duoc den dap."
@@ -26,6 +34,30 @@ def main(): # Hàm main
 
     def mon_toan(): # Toán
         mon = "Toan"
+        chon_hk(mon=mon)
+        def check_user_input_hk():
+            input_hk = input("Hay nhap so tuong ung: ")
+            if int(input_hk) == 1:
+                hk1_math()
+            elif int(input_hk) == 2:
+                hk2_math()
+            elif int(input_hk) == 3:
+                main()
+            else:
+                print("Vui long nhap so tuong ung 1 hoac 2.")
+                reset_input_math()
+        def reset_input_math():
+
+            check_user_input_hk()
+
+        check_user_input_hk()
+
+
+
+
+    def hk1_math():
+        mon = "Toan"
+        hk = " 1"
         # Tính điểm TX
         x_tx = 1
         tong_tx = 0
@@ -50,17 +82,76 @@ def main(): # Hàm main
         chia_tb = x_tx + tong_bai
 
         # Tính điểm TBC 
-        tbc = tong_tx + 2.0 * float(diem_gk) + 3.0 * float(diem_chk)
+        tbc = tong_tx + 2 * float(diem_gk) + 3 * float(diem_chk)
         tbm = tbc / chia_tb
-        print("Điểm trung binh cong mon " + mon + " cua ban là: " + str(tbm))
+        print("Điểm trung binh cong mon " + mon + hk + " cua ban là: " + str(tbm))
         xep_loai(mon=mon, tbm=tbm)
         print("")
         main()
 
 
 
-    def mon_van(): # Ngữ văn
+    def hk2_math():
+        mon = "Toan"
+        hk = " 2"
+        # Tính điểm TX
+        x_tx = 1
+        tong_tx = 0
+        so_cot_tx = input("Nhap so cot diem thuong xuyen ban co: ")
+
+        while x_tx < int(so_cot_tx):
+            input_cot_tx = input("Cot diem mieng so " + str(x_tx) + " cua ban là: ")
+            tong_tx = tong_tx + float(input_cot_tx)
+            x_tx = x_tx + 1
+
+        input_cot_tx = input("Cot diem mieng so " + str(x_tx) + " cua ban là: ")
+        tong_tx = tong_tx + float(input_cot_tx)
+
+        # Tính điểm GK
+        diem_gk = input("Nhap diem giua ki cua ban: ")
+
+        # Tính diểm CHK
+        diem_chk = input("Nhap diem cuoi ki cua ban: ")
+
+        # Tính tổng để chia
+        tong_bai = x_tx + 2
+        chia_tb = x_tx + tong_bai
+
+        # Tính điểm TBC 
+        tbc = tong_tx + 2 * float(diem_gk) + 3 * float(diem_chk)
+        tbm = tbc / chia_tb
+        print("Điểm trung binh cong mon " + mon + hk + " cua ban là: " + str(tbm))
+        xep_loai(mon=mon, tbm=tbm)
+        print("")
+        main()
+
+
+    def mon_van(): # Toán
         mon = "Ngu Van"
+        chon_hk(mon=mon)
+        def check_user_input_hk():
+            input_hk = input("Hay nhap so tuong ung: ")
+            if int(input_hk) == 1:
+                hk1_vie()
+            elif int(input_hk) == 2:
+                hk2_vie()
+            elif int(input_hk) == 3:
+                main()
+            else:
+                print("Vui long nhap so tuong ung 1 hoac 2.")
+                reset_input_vie()
+        def reset_input_vie():
+
+            check_user_input_hk()
+
+        check_user_input_hk()
+
+
+
+
+    def hk1_vie():
+        mon = "Ngu Van"
+        hk = " 1"
         # Tính điểm TX
         x_tx = 1
         tong_tx = 0
@@ -85,15 +176,18 @@ def main(): # Hàm main
         chia_tb = x_tx + tong_bai
 
         # Tính điểm TBC 
-        tbc = tong_tx + 2.0 * float(diem_gk) + 3.0 * float(diem_chk)
+        tbc = tong_tx + 2 * float(diem_gk) + 3 * float(diem_chk)
         tbm = tbc / chia_tb
-        print("Điểm trung binh cong mon " + mon + " cua ban là: " + str(tbm))
+        print("Điểm trung binh cong mon " + mon + hk + " cua ban là: " + str(tbm))
         xep_loai(mon=mon, tbm=tbm)
         print("")
         main()
 
-    def mon_anh():
-        mon = "Tieng Anh"
+
+
+    def hk2_vie():
+        mon = "Ngu Van"
+        hk = " 2"
         # Tính điểm TX
         x_tx = 1
         tong_tx = 0
@@ -118,9 +212,103 @@ def main(): # Hàm main
         chia_tb = x_tx + tong_bai
 
         # Tính điểm TBC 
-        tbc = tong_tx + 2.0 * float(diem_gk) + 3.0 * float(diem_chk)
+        tbc = tong_tx + 2 * float(diem_gk) + 3 * float(diem_chk)
         tbm = tbc / chia_tb
-        print("Điểm trung binh cong mon " + mon + " cua ban là: " + str(tbm))
+        print("Điểm trung binh cong mon " + mon + hk + " cua ban là: " + str(tbm))
+        xep_loai(mon=mon, tbm=tbm)
+        print("")
+        main()
+
+
+    def mon_anh(): # Toán
+        mon = "Tieng Anh"
+        chon_hk(mon=mon)
+        def check_user_input_hk():
+            input_hk = input("Hay nhap so tuong ung: ")
+            if int(input_hk) == 1:
+                hk1_english()
+            elif int(input_hk) == 2:
+                hk2_english()
+            elif int(input_hk) == 3:
+                main()
+            else:
+                print("Vui long nhap so tuong ung 1 hoac 2.")
+                reset_input_english()
+        def reset_input_english():
+
+            check_user_input_hk()
+
+        check_user_input_hk()
+
+
+
+
+    def hk1_english():
+        mon = "Tieng Anh"
+        hk = " 1"
+        # Tính điểm TX
+        x_tx = 1
+        tong_tx = 0
+        so_cot_tx = input("Nhap so cot diem thuong xuyen ban co: ")
+
+        while x_tx < int(so_cot_tx):
+            input_cot_tx = input("Cot diem mieng so " + str(x_tx) + " cua ban là: ")
+            tong_tx = tong_tx + float(input_cot_tx)
+            x_tx = x_tx + 1
+
+        input_cot_tx = input("Cot diem mieng so " + str(x_tx) + " cua ban là: ")
+        tong_tx = tong_tx + float(input_cot_tx)
+
+        # Tính điểm GK
+        diem_gk = input("Nhap diem giua ki cua ban: ")
+
+        # Tính diểm CHK
+        diem_chk = input("Nhap diem cuoi ki cua ban: ")
+
+        # Tính tổng để chia
+        tong_bai = x_tx + 2
+        chia_tb = x_tx + tong_bai
+
+        # Tính điểm TBC 
+        tbc = tong_tx + 2 * float(diem_gk) + 3 * float(diem_chk)
+        tbm = tbc / chia_tb
+        print("Điểm trung binh cong mon " + mon + hk + " cua ban là: " + str(tbm))
+        xep_loai(mon=mon, tbm=tbm)
+        print("")
+        main()
+
+
+
+    def hk2_english():
+        mon = "Tieng Anh"
+        hk = " 2"
+        # Tính điểm TX
+        x_tx = 1
+        tong_tx = 0
+        so_cot_tx = input("Nhap so cot diem thuong xuyen ban co: ")
+
+        while x_tx < int(so_cot_tx):
+            input_cot_tx = input("Cot diem mieng so " + str(x_tx) + " cua ban là: ")
+            tong_tx = tong_tx + float(input_cot_tx)
+            x_tx = x_tx + 1
+
+        input_cot_tx = input("Cot diem mieng so " + str(x_tx) + " cua ban là: ")
+        tong_tx = tong_tx + float(input_cot_tx)
+
+        # Tính điểm GK
+        diem_gk = input("Nhap diem giua ki cua ban: ")
+
+        # Tính diểm CHK
+        diem_chk = input("Nhap diem cuoi ki cua ban: ")
+
+        # Tính tổng để chia
+        tong_bai = x_tx + 2
+        chia_tb = x_tx + tong_bai
+
+        # Tính điểm TBC 
+        tbc = tong_tx + 2 * float(diem_gk) + 3 * float(diem_chk)
+        tbm = tbc / chia_tb
+        print("Điểm trung binh cong mon " + mon + hk + " cua ban là: " + str(tbm))
         xep_loai(mon=mon, tbm=tbm)
         print("")
         main()
@@ -164,16 +352,9 @@ def main(): # Hàm main
 
 
 
-
-
-
     print("Vui long chon loai mon hoc:")
     chon_mon()
     user_input()
-
-
-
-
 
 
 
